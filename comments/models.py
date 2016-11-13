2e8be034649c7668b8508aad3cc13529c9fda909
Mikhail Kuznetsov <mkuznetsov.dev@gmail.com>
2016-11-13 21:15:29 +0300

    comment model;

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import Truncator
 
-# Create your models here.
+from core.models import TimeStampedModel
+from ratings.models import UpDownRating, BaseVote
+
+
+class Comment(TimeStampedModel, models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+    publication = models.ForeignKey('publications.Publication', related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return '{id} - {text}'.format(id=self.pk, text=Truncator(self.title).chars(50))
+
+
+class CommentRating(UpDownRating):
+    ITEM_CLASS = Comment
+
+
+class CommentVote(BaseVote):
+    RATING_CLASS = CommentRating
